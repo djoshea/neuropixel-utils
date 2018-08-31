61b71befbc7d6ac709e5b79691f48731a19970dc
Dan O'Shea <dan@djoshea.com>
2018-08-30 19:48:05 -0700

    Improving example script

--- trialDataExample.m
+++ trialDataExample.m
@@ -1,19 +1,28 @@
 % dsPath = '/Volumes/pixin/will_sort/Pierre_20180609_08';
-dsPath = '/path/to/kilosort_output';
+kilosortDatasetPath = '/path/to/kilosort_output';
 
-ds = KiloSort.Dataset(dsPath);
+% build path to channel map file
+channelMapFile '/path/to/neuropixel-utils/map_files/neuropixPhase3A_kilosortChanMap.mat');
+
+% load the kilosort dataset
+ds = KiloSort.Dataset(kilosortDatasetPath, channelMapFile);
 ds.load();
 
 % Figure out where trials start and stop
-% tsi is TrialSegmentationInfo
-% load hopefully cached trialSegmentationInfo.mat file, else parse sync from raw imec.ap.bin file
+% tsi is TrialSegmentationInfo that stores this info
+% this will load trialSegmentationInfo.mat file if present, but the first time will parse the sync from raw imec.ap.bin file
 imecAPBinFile = '/path/to/data.imec.ap.bin';
 tsi = NeuropixelExpt.DataLoad.readTrialInfo(imecAPBinFile);
 
 % trial_ids is the list of trial ids for the behavioral data you want to merge into
 % e.g. from TrialData object
 trial_ids = td.getParam('trialId');
+
+% segment the kilosort dataset into trials, with the final arrays matching
+% trial_ids (and missing trials being empty)
+% so seg will have numel(trial_ids) trials in it regardless of how many trials are found in the ImecDataFile
 seg = KiloSort.TrialSegmentedDataset(ds08, tsi, trial_ids);
 
+% merge into trial data
 td = td.dropSpikeChannels();
 td = seg.addSpikesToTrialData(td, 'npix');
